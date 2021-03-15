2f074a79faf037913140df27071c457e951e3fc2
ACOIDAN MESA HERNANDEZ <alu0101026479@ull.edu.es>
2021-03-15 11:20:41 +0000

    Estructura, documentación y tests generados

---
+++ test/ejercicio-1.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import {expect} from 'chai';
+import {} from '../src/ejercicio-1';
+
+describe('Prueba de ejercicio 1', () => {
+  it('', () => {
+    expect().to.be.equal();
+  });
+});
