4aa2b87d7fce80c032251af5f596df807bbfc68c
ACOIDAN MESA HERNANDEZ <alu0101026479@ull.edu.es>
2021-03-17 19:39:41 +0000

    Funcionando la referencia a un artículo en formato APA (Ejercicio 2)

--- src/ejercicio-2/articulo.ts
+++ src/ejercicio-2/articulo.ts
@@ -1,5 +1,5 @@
 export class Articulo {
-  constructor(private titulo: string, private autor: string | string[], private emailAutor: string | string[], private keywords: string | string[], private resumen: string, private fechaPublicacion: string, private editorial: string, private numeroCitas: number) {}
+  constructor(private titulo: string, private autor: string[], private emailAutor: string[], private keywords: string[], private resumen: string, private fechaPublicacion: string, private editorial: string, private numeroCitas: number) {}
 
   public getTitulo() {
     return this.titulo;
@@ -31,5 +31,27 @@ export class Articulo {
 
   public getNumeroCitas() {
     return this.numeroCitas;
+  }
+
+  public referencia() {
+    let salida: string = "";
+    let i = 0;
+    this.autor.forEach((autor) => {
+      if (i == 0) {
+        salida = autor;
+      }
+      if (i > 0) {
+        if (i == this.autor.length-1) {
+          salida = salida + ` y ${autor}. `;
+        } else {
+          salida = salida + `, ${autor}.`;
+        }
+      }
+      i++;
+    });
+
+    salida = salida + `(${this.fechaPublicacion}) "${this.titulo}", ${this.editorial}`;
+
+    return salida;
   }
 }
--- tests/ejercicio-2.spec.ts
+++ tests/ejercicio-2.spec.ts
@@ -3,9 +3,9 @@ import {expect} from 'chai';
 import {Articulo} from '../src/ejercicio-2/articulo';
 
 describe('Pruebas del Ejercicio 2 - Gestor Bibliográfico', () => {
-  const articulo1 = new Articulo("Lionel Messi se lesiono", "ACOIDAN MESA HERNANDEZ", "alu0101206479@ull.edu.es", ["Messi", "Lesionado"], "Messi se ha lesionado en el partido contra el Real Madrid", "21.10.2000", "MiCasa", 3);
+  const articulo1 = new Articulo("Lionel Messi se lesiono", ["ACOIDAN MESA HERNANDEZ"], ["alu0101206479@ull.edu.es"], ["Messi", "Lesionado"], "Messi se ha lesionado en el partido contra el Real Madrid", "21.10.2000", "MiCasa", 3);
   const articulo2 = new Articulo("Cristiano Ronaldo se lesiono", ["ACOIDAN MESA HERNANDEZ", "JORGE GARCIA BORGES"], ["alu0101206479@ull.edu.es", "alu0101207489@ull.edu.es"], ["Cristiano", "Lesionado"], "Cristiano Ronaldo se ha lesionado en un partido contra el Getafe", "21.11.2001", "MiCasa", 5);
-  const articulo3 = new Articulo("ElRubiusOMG en el ojo público", "JORGE GARCIA BORGES", "alu0101207489@ull.edu.es", ["Rubius", "Youtube", "Andorra"], "El youtuber más famoso de todos (ElRubiusOMG) se marcha a Andorra para no pagar tantos impuestos y es criticado por la poblaciñon española", "21.10.2000", "Scaella", 10);
+  const articulo3 = new Articulo("ElRubiusOMG en el ojo público", ["JORGE GARCIA BORGES"], ["alu0101207489@ull.edu.es"], ["Rubius", "Youtube", "Andorra"], "El youtuber más famoso de todos (ElRubiusOMG) se marcha a Andorra para no pagar tantos impuestos y es criticado por la poblaciñon española", "21.10.2000", "Scaella", 10);
   describe('Clase Articulo', () => {
     describe('Se puede instanciar un artículo', () => {
       it('expect(articulo1).not.be.equal(null);', () => {
@@ -20,15 +20,15 @@ describe('Pruebas del Ejercicio 2 - Gestor Bibliográfico', () => {
     });
   });
 
-  describe('Funcionan los getters de artículo', () => {
+  describe('Funcionan los getters', () => {
     it('expect(articulo1.getTitulo()).to.be.equal("Lionel Messi se lesiono");', () => {
       expect(articulo1.getTitulo()).to.be.equal("Lionel Messi se lesiono");
     });
-    it('expect(articulo1.getAutor()).to.be.equal("ACOIDAN MESA HERNANDEZ");', () => {
-      expect(articulo1.getAutor()).to.be.equal("ACOIDAN MESA HERNANDEZ");
+    it('expect(articulo1.getAutor()).to.be.equal(["ACOIDAN MESA HERNANDEZ"]);', () => {
+      expect(articulo1.getAutor()).to.deep.equal(["ACOIDAN MESA HERNANDEZ"]);
     });
-    it('expect(articulo1.getEmailAutor()).to.be.equal("alu0101206479@ull.edu.es");', () => {
-      expect(articulo1.getEmailAutor()).to.be.equal("alu0101206479@ull.edu.es");
+    it('expect(articulo1.getEmailAutor()).to.be.equal(["alu0101206479@ull.edu.es");', () => {
+      expect(articulo1.getEmailAutor()).to.deep.equal(["alu0101206479@ull.edu.es"]);
     });
     it('expect(articulo1.getKeywords()).to.deep.equal(["Messi", "Lesionado"]);', () => {
       expect(articulo1.getKeywords()).to.deep.equal(["Messi", "Lesionado"]);
@@ -42,8 +42,11 @@ describe('Pruebas del Ejercicio 2 - Gestor Bibliográfico', () => {
     it('expect(articulo1.getEditorial()).to.be.equal("MiCasa");', () => {
       expect(articulo1.getEditorial()).to.be.equal("MiCasa");
     });
-    it('expect(expect(Venusaur.getEstadisticasBasicas().hp).to.be.equal(80);', () => {
-      expect(articulo1.getNumeroCitas()).to.be.equal(3);
+  });
+
+  describe('Funciona la función de referencia de artículo en formato APA', () => {
+    it(`expect(articulo2.referencia()).to.be.equal('ACOIDAN MESA HERNANDEZ y JORGE GARCIA BORGES. (21.11.2001) "Cristiano Ronaldo se lesiono", MiCasa);`, () => {
+      expect(articulo2.referencia()).to.be.equal('ACOIDAN MESA HERNANDEZ y JORGE GARCIA BORGES. (21.11.2001) "Cristiano Ronaldo se lesiono", MiCasa');
     });
   });
 });
